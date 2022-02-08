737a6da52f65bacead71d2f5941facf9bd8e1328
joaovclobo <joao-lobo12@hotmail.com>
2022-02-08 16:35:57 -0300

    limpeza

--- main.c
+++ main.c
@@ -14,16 +14,6 @@ int main()
     capacidadeCaminhaoPtr = &capacidadeCaminhao;
     matrizDistancias = lerarquivo(nomeArquivo, totalCidadesptr, capacidadeCaminhaoPtr, listadeDemandas);
 
-    // printf("%d\n%d\n", totalCidades, capacidadeCaminhao);
-
-    // for (int i = 0; i < totalCidades; ++i) {
-    //     printf("%d ", listadeDemandas[i]);
-    // }
-
-    // printf("\n");
-
-    // imprimeMatriz(totalCidades, matrizDistancias);
-
     int *cidadesNpermutadas, j = 1;
 
     cidadesNpermutadas = (int*) malloc(totalCidades*sizeof(int));
@@ -31,12 +21,7 @@ int main()
     for (int i = 1; i < totalCidades; i++){
         cidadesNpermutadas[i-1] = i;
     }
-
     cidadesNpermutadas[totalCidades-1] = -1;
-
-    // for (int i = 0; i < totalCidades; ++i) {
-    //     printf("%d ", cidadesNpermutadas[i]);
-    // }
 
     while (cidadesNpermutadas[j - 1] != -1)
     {
