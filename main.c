8a09b264aa02a80ff87b173d8cb9ac566fb82159
Michel Castro <42982212+mixchel@users.noreply.github.com>
2022-02-08 17:54:04 -0300

    Revert "Adicionei remover cidades no main"
    This reverts commit 80fcc937999ca0864d28fd1d984ace6a36b44564.

--- main.c
+++ main.c
@@ -11,29 +11,22 @@ int main()
     int totalCidades, capacidadeCaminhao;
     int listadeDemandas[1000];
     int *cidadesNaoPerm, j = 1;
-    int Qtdcaminhoes = 0, Tamanho_Vetor_Cidade;
     
     totalCidadesptr = &totalCidades;
     capacidadeCaminhaoPtr = &capacidadeCaminhao;
     matrizDistancias = lerarquivo(nomeArquivo, totalCidadesptr, capacidadeCaminhaoPtr, listadeDemandas);
-    
+
     cidadesNaoPerm = (int*) malloc(totalCidades*sizeof(int));
-    for (int i = 0; i < totalCidades; i++){
-        Qtdcaminhoes += listadeDemandas[i];
-    }
-    Qtdcaminhoes /= capacidadeCaminhao;
+
     for (int i = 1; i < totalCidades; i++){
         cidadesNaoPerm[i-1] = i;
     }
     cidadesNaoPerm[totalCidades-1] = -1;
-    Tamanho_Vetor_Cidade = totalCidades - 1;
-    for (int i = 0; i < Qtdcaminhoes; i++){
-        while (cidadesNaoPerm[j - 1] != -1)
-        {
-            geraPetala(j, cidadesNaoPerm, capacidadeCaminhao, listadeDemandas, matrizDistancias);
-            j++;
-        }
-        cidadesNaoPerm = RemoveCidades(cidadesNaoPerm, petala_atual, Tamanho_Vetor_Cidade, CalcularTamanhoPetala(petala_atual));     
-        Tamanho_Vetor_Cidade = CalcularTamanhoVetorCidade(cidadesNaoPerm);
-    }
+
+    while (cidadesNaoPerm[j - 1] != -1)
+    {
+        geraPetala(j, cidadesNaoPerm, capacidadeCaminhao, listadeDemandas, matrizDistancias);
+        j++;
+    }        
+
 }
